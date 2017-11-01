c13f5d718d967070c06a4f949a18cf76c9701071
Andrew Shelyakov <andrew.shelyakov@ya.ru>
2017-11-01 22:28:29 +0300

    Сlear the class name before making a api call in wrapper

--- src/Wrapper.php
+++ src/Wrapper.php
@@ -3,22 +3,21 @@ namespace VK;
 
 class Wrapper {
 	private $apiInstance;
-	private $method;
+	private $class;
 	
 	public function __construct ($apiInstance) {
 		$this->apiInstance = $apiInstance;
 	}
 
-	public function __get (string $method) {
-		$this->method = $method;
+	public function __get (string $class) {
+		$this->class = $class;
 		return $this;
 	}
 
 	public function __call (string $method, array $parameters = []) {
-		$method = $this->method . '.' . $method;
+		$method = $this->class . '.' . $method;
+		$this->class = NULL;
 		$parameters = $parameters[0] ?? [];
-		$response = $this->apiInstance->call ($method, $parameters);
-		$this->method = NULL;
-		return $response;
+		return $this->apiInstance->call ($method, $parameters);
 	}
 }
